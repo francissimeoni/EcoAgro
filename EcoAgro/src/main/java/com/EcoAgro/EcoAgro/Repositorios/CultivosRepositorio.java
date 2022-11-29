92b63a868b520989415c4025382b43b03f9bfd92
Francis Simeoni <francissimeoni90@gmail.com>
2022-11-28 21:06:44 -0300

    al dia con los thymeleaf y fragments, demo 3

--- EcoAgro/src/main/java/com/EcoAgro/EcoAgro/Repositorios/CultivosRepositorio.java
+++ EcoAgro/src/main/java/com/EcoAgro/EcoAgro/Repositorios/CultivosRepositorio.java
@@ -1,10 +1,16 @@
 package com.EcoAgro.EcoAgro.Repositorios;
 
 import com.EcoAgro.EcoAgro.Entidades.Cultivos;
+import com.EcoAgro.EcoAgro.Entidades.Usuarios;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CultivosRepositorio extends JpaRepository<Cultivos, String> {
+
+   // @Query("select c from Cultivos c limit 0,2")
+    //public Cultivos traerCultivos();
 
 }
